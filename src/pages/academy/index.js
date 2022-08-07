ebeea85dec6e05c6c1ab9b08bac93c0d67787867
nazmul-7 <nazmulchowdhury4@gmail.com>
2022-08-07 21:42:01 +0600

    from nazmul

--- src/pages/academy/index.js
+++ src/pages/academy/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Academy from '../../components/academy/Academy';
+import Head from 'next/head'
 
 const index = () => {
   return (
--- src/pages/bootcamp/[_slug]/index.js
+++ src/pages/bootcamp/[_slug]/index.js
@@ -1,6 +1,7 @@
 
 import React, { useState } from 'react';
 import Bootcamp from '../../../components/bootcamp/Bootcamp';
+import Head from 'next/head'
 
 
 const Index = () => {
