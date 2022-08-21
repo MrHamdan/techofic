78d5781382a60fd20e57fcba21c3b9a22dfcd6af
nazmul-7 <nazmulchowdhury4@gmail.com>
2022-08-21 17:55:47 +0600

    from nazmul

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const HTTP_ENDPOINT = "https://apiv1.techofic.com";
-export const HTTP_ENDPOINT = "http://127.0.0.1:3333";
+export const HTTP_ENDPOINT = "https://apiv1.techofic.com";
+// export const HTTP_ENDPOINT = "http://127.0.0.1:3333";
